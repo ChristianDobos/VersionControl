7f1b900490dfae51d28894cc7230f972d9358100
Krisztián Dobos <krisztian.dobos@stud.uni-corvinus.hu>
2020-10-18 15:26:07 +0200

    Initial commit

---
+++ Excel generálás/Excel generálás/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace Excel_generálás
+{
+    
+    public partial class Form1 : Form
+    {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+
+            try
+            {
+                xlApp = new Excel.Application();
+                xlSheet = xlWb.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
+        }
+
+        public void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+
+        public void CreateTable()
+        {
+            string[] headers = new string[] {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
+
+            xlSheet.Cells[1, 1] = headers[0];
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 8] = "";
+                counter++;
+            }
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+
+        }
+    }
+}
